b7ef7b0b68a0eca1fd8265c46d3876b01b3dd24d
changming <changming@kugou.net>
2017-07-31 15:37:39 +0800

    1、优化monkey逻辑

--- src/main/java/com/ios/monkey/Monkey.java
+++ src/main/java/com/ios/monkey/Monkey.java
@@ -20,7 +20,7 @@ public class Monkey {
     private int width, height, submitX_mim, submitX_max, submitY_mim, submitY_max, contentX_min, contentX_max, contentY_mim, contentY_max, special_point_x, special_point_y;
     private static boolean needhelp = false;
     private static String UDID, BUNDLEID, APPPATH, RESULT_DIR;
-    
+
     // 默认允许180分钟
     private static String TIMING = "180";
     private static String PORT = "3456";
@@ -48,7 +48,7 @@ public class Monkey {
             } else if ("-b".equals(args[optSetting])) {
                 BUNDLEID = args[++optSetting];
             } else if ("-t".equals(args[optSetting])) {
-            	TIMING = args[++optSetting];
+                TIMING = args[++optSetting];
             } else if ("-d".equals(args[optSetting])) {
                 RESULT_DIR = args[++optSetting];
             } else if ("-host".equals(args[optSetting])) {
@@ -56,20 +56,20 @@ public class Monkey {
             } else if ("-reuse".equals(args[optSetting])) {
                 REUSE = args[++optSetting];
             } else if ("-port".equals(args[optSetting])) {
-            	PORT = args[++optSetting];
+                PORT = args[++optSetting];
             } else if ("-proxyport".equals(args[optSetting])) {
                 PROXYPORT = args[++optSetting];
             } else if ("-h".equals(args[optSetting])) {
                 needhelp = true;
                 System.out.println(
                         "-u:设备的UDID\n" +
-                        "-a:测试App的绝对路径\n"+
-                        "-b:测试App的Bundle\n"+
-                        "-host:macaca服务器host\n"+
-                        "-reuse:0: 启动并安装 app。1 (默认): 卸载并重装 app。 2: 仅重装 app。3: 在测试结束后保持 app 状态。\n"+
-                        "-t:运行时长（分），可选，默认180分钟\n"+
-                        "-port:macaca服务的端口，默认3456\n" +
-                        "-proxyport:usb代理端口，默认8900");
+                                "-a:测试App的绝对路径\n"+
+                                "-b:测试App的Bundle\n"+
+                                "-host:macaca服务器host\n"+
+                                "-reuse:0: 启动并安装 app。1 (默认): 卸载并重装 app。 2: 仅重装 app。3: 在测试结束后保持 app 状态。\n"+
+                                "-t:运行时长（分），可选，默认180分钟\n"+
+                                "-port:macaca服务的端口，默认3456\n" +
+                                "-proxyport:usb代理端口，默认8900");
                 break;
             }
 
@@ -80,20 +80,20 @@ public class Monkey {
                 org.testng.Assert.assertTrue((!UDID.equals(null)) && (!BUNDLEID.equals(null)));
             } catch (Exception e) {
                 System.out.println("请确认参数配置,需要帮助请输入 java -jar iosMonkey.jar -h\n"
-                		+ "ERROR信息"+ e.toString());
+                        + "ERROR信息"+ e.toString());
             }
-            
+
             try {
-            	new Monkey().run();
+                new Monkey().run();
             } catch (Exception e) {
-            	e.printStackTrace();
+                e.printStackTrace();
             }
         }
     }
 
 
     private void run() throws Exception {
-    	System.out.println("本次设定的运行时长【" + TIMING + "】分钟");
+        System.out.println("本次设定的运行时长【" + TIMING + "】分钟");
         init();
         width = (Integer) driver.getWindowSize().get("width");
         height = (Integer) driver.getWindowSize().get("height");
@@ -114,9 +114,7 @@ public class Monkey {
 
         // 卸载安装需要划过闪屏页
         if(REUSE!="3"){
-            driver.acceptAlert();
-            Thread.sleep(3000);
-            driver.acceptAlert();
+
             Thread.sleep(3000);
             double startX = Math.ceil(0.8 * (width - 1));
             double startY = Math.ceil(0.5 * (height - 1));
@@ -124,90 +122,102 @@ public class Monkey {
             double endY = Math.ceil(0.5 * (height - 1));
             new MonkeySpecialEvent(driver, startX, startY, endX, endY).injectEvent();
         }
+        long lastTime = (Integer.parseInt(TIMING) * 60 * 1000);
         long startTime = System.currentTimeMillis();
         while (true) {
-            switch (new MathRandom().PercentageRandom()) {
-                case 0: {
-                    double x = Math.ceil(Math.random() * (width - 1));
-                    double y = Math.ceil(Math.random() * (height - 1));
-                    
-                    Screenshot.screenshot(RESULT_DIR, UDID, new Double(x).intValue(), new Double(y).intValue());
-                    new MonkeyTapEvent(driver, x, y).injectEvent();
-                    eventcount = eventcount+1;
-                    System.out.println("---EVENT执行了："+eventcount+"次---");
-                    break;
-                }
-                case 1: {
-                	double startX = Math.ceil(Math.random() * (width - 1));
-                    double startY = Math.ceil(Math.random() * (height - 1));
-                    double endX = Math.ceil(Math.random() * (width - 1));
-                    double endY = Math.ceil(Math.random() * (height - 1));
-                    
-                	Screenshot.screenshot(RESULT_DIR, UDID, new Double(startX).intValue(), new Double(endY).intValue());
-                    new MonkeySwipeEvent(driver, startX, startY, endX, endY).injectEvent();
-                    eventcount = eventcount+1;
-                    System.out.println("---EVENT执行了："+eventcount+"次---");
-                    break;
-                }
-                case 2: {
-                	Screenshot.screenshot(RESULT_DIR, UDID, backX, backY);
-                    new MonkeyBackEvent(driver, backX, backY).injectEvent();
-                    eventcount = eventcount+1;
-                    System.out.println("---EVENT执行了："+eventcount+"次---");
-                    break;
-                }
-                case 3: {
-                	Random random = new Random();
-                    int x = random.nextInt(submitX_max) % (submitX_max - submitX_mim + 1) + submitX_mim;
-                    int y = random.nextInt(submitY_max) % (submitY_max - submitY_mim + 1) + submitY_mim;
-                	
-                	Screenshot.screenshot(RESULT_DIR, UDID, x, y);
-                    new MonkeySubmitEvent(driver, x, y).injectEvent();
-                    eventcount = eventcount+1;
-                    System.out.println("---EVENT执行了："+eventcount+"次---");
-                    break;
-                }
-                case 4: {
-                    Random random = new Random();
-                    int x = random.nextInt(contentX_max) % (contentX_max - contentX_min + 1) + contentX_min;
-                    int y = random.nextInt(contentY_max) % (contentY_max - contentY_mim + 1) + contentY_mim;
-                    
-                	Screenshot.screenshot(RESULT_DIR, UDID, x, y);
-                    new MonkeyContentEvent(driver, x, y).injectEvent();
-                    eventcount = eventcount+1;
-                    System.out.println("---EVENT执行了："+eventcount+"次---");
-                    break;
-                }
-                case 5: {
-                	Screenshot.screenshot(RESULT_DIR, UDID, special_point_x, special_point_y);
-                    new MonkeyTapSpecialPointEvent(driver, special_point_x, special_point_y).injectEvent();
-                    eventcount = eventcount+1;
-                    System.out.println("---EVENT执行了："+eventcount+"次---");
-                    break;
-                }
-                case 6: {
-                    new MonkeyHomeKeyEvent(driver,UDID,BUNDLEID).injectEvent();
-                    eventcount = eventcount+1;
-                    System.out.println("---EVENT执行了："+eventcount+"次---");
-                    break;
+            long endTime = System.currentTimeMillis();
+            try{
+                switch (new MathRandom().PercentageRandom()) {
+                    case 0: {
+                        double x = Math.ceil(Math.random() * (width - 1));
+                        double y = Math.ceil(Math.random() * (height - 1));
+
+                        Screenshot.screenshot(RESULT_DIR, UDID, new Double(x).intValue(), new Double(y).intValue());
+                        new MonkeyTapEvent(driver, x, y).injectEvent();
+                        eventcount = eventcount + 1;
+                        System.out.println("---EVENT执行了：" + eventcount + "次---");
+                        break;
+                    }
+                    case 1: {
+                        double startX = Math.ceil(Math.random() * (width - 1));
+                        double startY = Math.ceil(Math.random() * (height - 1));
+                        double endX = Math.ceil(Math.random() * (width - 1));
+                        double endY = Math.ceil(Math.random() * (height - 1));
+
+                        Screenshot.screenshot(RESULT_DIR, UDID, new Double(startX).intValue(), new Double(endY).intValue());
+                        new MonkeySwipeEvent(driver, startX, startY, endX, endY).injectEvent();
+                        eventcount = eventcount + 1;
+                        System.out.println("---EVENT执行了：" + eventcount + "次---");
+                        break;
+                    }
+                    case 2: {
+                        Screenshot.screenshot(RESULT_DIR, UDID, backX, backY);
+                        new MonkeyBackEvent(driver, backX, backY).injectEvent();
+                        eventcount = eventcount + 1;
+                        System.out.println("---EVENT执行了：" + eventcount + "次---");
+                        break;
+                    }
+                    case 3: {
+                        Random random = new Random();
+                        int x = random.nextInt(submitX_max) % (submitX_max - submitX_mim + 1) + submitX_mim;
+                        int y = random.nextInt(submitY_max) % (submitY_max - submitY_mim + 1) + submitY_mim;
+
+                        Screenshot.screenshot(RESULT_DIR, UDID, x, y);
+                        new MonkeySubmitEvent(driver, x, y).injectEvent();
+                        eventcount = eventcount + 1;
+                        System.out.println("---EVENT执行了：" + eventcount + "次---");
+                        break;
+                    }
+                    case 4: {
+                        Random random = new Random();
+                        int x = random.nextInt(contentX_max) % (contentX_max - contentX_min + 1) + contentX_min;
+                        int y = random.nextInt(contentY_max) % (contentY_max - contentY_mim + 1) + contentY_mim;
+
+                        Screenshot.screenshot(RESULT_DIR, UDID, x, y);
+                        new MonkeyContentEvent(driver, x, y).injectEvent();
+                        eventcount = eventcount + 1;
+                        System.out.println("---EVENT执行了：" + eventcount + "次---");
+                        break;
+                    }
+                    case 5: {
+                        Screenshot.screenshot(RESULT_DIR, UDID, special_point_x, special_point_y);
+                        new MonkeyTapSpecialPointEvent(driver, special_point_x, special_point_y).injectEvent();
+                        eventcount = eventcount + 1;
+                        System.out.println("---EVENT执行了：" + eventcount + "次---");
+                        break;
+                    }
+                    case 6: {
+                        new MonkeyHomeKeyEvent(driver, UDID, BUNDLEID).injectEvent();
+                        eventcount = eventcount + 1;
+                        System.out.println("---EVENT执行了：" + eventcount + "次---");
+                        break;
+                    }
                 }
             }
-            
-        	long endTime = System.currentTimeMillis();
-        	if((endTime - startTime) > (Integer.parseInt(TIMING) * 60 * 1000)) {
-        		System.out.println("已运行" + (endTime - startTime)/60/1000 + "分钟，任务即将结束");
-        		break;
-        	}
-
+            catch (Exception e) {
+                driver.quit();
+                init();
+                lastTime -= endTime-startTime;
+                startTime = System.currentTimeMillis();
+                System.out.println("*******************************************\n\n\n" +
+                        "请在命令行输入 macaca server --verbose 启动服务\n\n\n" +
+                        "*******************************************\n");
+            }
+            if((endTime - startTime) > lastTime) {
+                System.out.println("已运行" + (endTime - startTime)/60/1000 + "分钟，任务即将结束");
+                break;
+            }
         }
+
         driver.quit();
     }
+
 
     private void init() throws IOException, InterruptedException {
         driver = new MacacaClient();
         JSONObject porps = new JSONObject();
         porps.put("platformName", "ios");
-        porps.put("reuse", Integer.parseInt(REUSE));
+        porps.put("reuse", 3);
         porps.put("bundleId", BUNDLEID);
         porps.put("app", APPPATH);
         porps.put("udid", UDID);
