96b83d482ea95d1e7ad162f576e5e1b08a155d9e
changming <changming@kugou.net>
2017-08-03 09:22:15 +0800

    1、优化代码逻辑

--- src/main/java/com/ios/monkey/Monkey.java
+++ src/main/java/com/ios/monkey/Monkey.java
@@ -20,7 +20,6 @@ public class Monkey {
     private int width, height, submitX_mim, submitX_max, submitY_mim, submitY_max, contentX_min, contentX_max, contentY_mim, contentY_max, special_point_x, special_point_y;
     private static boolean needhelp = false;
     private static String UDID, BUNDLEID, APPPATH, RESULT_DIR;
-
     // 默认允许180分钟
     private static String TIMING = "180";
     private static String PORT = "3456";
@@ -95,8 +94,14 @@ public class Monkey {
     private void run() throws Exception {
         System.out.println("本次设定的运行时长【" + TIMING + "】分钟");
         init();
-        width = (Integer) driver.getWindowSize().get("width");
-        height = (Integer) driver.getWindowSize().get("height");
+        driver.acceptAlert();
+        Thread.sleep(3000);
+        driver.acceptAlert();
+        Thread.sleep(3000);
+        JSONObject rect = new JSONObject();
+        rect = driver.getWindowSize();
+        width = (Integer) rect.get("width");
+        height = (Integer) rect.get("height");
         NumberFormat numberFormat = NumberFormat.getInstance();
         numberFormat.setMaximumFractionDigits(2);
 
